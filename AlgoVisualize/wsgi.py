6129a47bf60af8bd3815c1d7759583468e254792
Hussnain47 <hussnainilyas2344@gmail.com>
2021-07-21 03:28:02 +0500

    a

--- AlgoVisualize/wsgi.py
+++ AlgoVisualize/wsgi.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from AlgoVisualize import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AlgoVisualize.settings')
 
 application = get_wsgi_application()
